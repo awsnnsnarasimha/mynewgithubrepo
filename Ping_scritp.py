0cefb466e6c824e2b3ea3c787e62ee887e2d4522
awsnnsnarasimha <132322846+awsnnsnarasimha@users.noreply.github.com>
2023-05-03 08:48:31 +0530

    icmp
    icmp packet file to test target device

---
+++ Ping_scritp.py
@@ -0,0 +1,12 @@
+### Ping script for Network Devices##
+
+import subprocess
+import platform
+
+def ping (host):
+    param ='-n' if platform.system().lower() == 'windows' #else '-c'
+    #command =['ping', '-c', '1', host]
+    return subprocess.call(command)
+
+host='google.com'
+print(ping(host))
